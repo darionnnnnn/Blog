7a1e464cb3972e9831adc9935c45f6be10cc2ae8
Wayne <wayneeeeee@gmail.com>
2021-01-23 22:12:31 +0800

    Design Pattern Behavioral_03_Interpreter

---
+++ Blog/Behavioral_03_Interpreter/Default.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioral_03_Interpreter
+{
+    public class Default
+    {
+        /// <summary>
+        /// 由直譯器來處理的內容
+        /// </summary>
+        public class Context
+        {
+        }
+
+        /// <summary>
+        /// 直譯的抽象類別或介面
+        /// </summary>
+        public abstract class AbstractExpression
+        {
+            public abstract void Interpret(Context context);
+        }
+
+        /// <summary>
+        /// 直譯邏輯實作，已無接續項目，不再遞迴
+        /// </summary>
+        public class TerminalExpression : AbstractExpression
+        {
+            public override void Interpret(Context context)
+            {
+                Console.WriteLine("Called Terminal.Interpret()");
+            }
+        }
+
+        /// <summary>
+        /// 直譯邏輯實作，有接續項目
+        /// </summary>
+        public class NonterminalExpression : AbstractExpression
+        {
+            public override void Interpret(Context context)
+            {
+                Console.WriteLine("Called Nonterminal.Interpret()");
+            }
+        }
+    }
+}
---
+++ Blog/Behavioral_03_Interpreter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Behavioral_03_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Default.Context context = new Default.Context();
+
+            ArrayList list = new ArrayList();
+
+            list.Add(new Default.TerminalExpression());
+            list.Add(new Default.NonterminalExpression());
+            list.Add(new Default.TerminalExpression());
+            list.Add(new Default.TerminalExpression());
+
+            foreach (Default.AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
