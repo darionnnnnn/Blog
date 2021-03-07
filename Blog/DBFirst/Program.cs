6e3cbcf22c975acafd52ecc06bbc9f398b7462f7
Wayne <wayneeeeee@gmail.com>
2021-03-07 11:37:38 +0800

    Putting some comments in code.

--- Blog/DBFirst/Program.cs
+++ Blog/DBFirst/Program.cs
@@ -13,12 +13,15 @@ namespace DBFirst
         {
             var connectionString = @"Persist Security Info=False;User ID=[loginId];Password=[Password];Initial Catalog=Northwind;Server=[DB_IP]";
 
+            // 透過 .AddDbContext 將 Northwind 註冊到 DI 容器
             await using (var services = new ServiceCollection()
                 .AddDbContext<Northwind>(options => options.UseSqlServer(connectionString))
                 .BuildServiceProvider())
             {
+                // 透過 DI 容器取得物件
                 var dbNorthwind = services.GetRequiredService<Northwind>();
 
+                // 取得全部 employee 資料
                 var employees = await dbNorthwind.Employees.ToListAsync();
 
                 Console.WriteLine("Employee Info");
